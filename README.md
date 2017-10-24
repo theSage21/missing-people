d05620e481eeb05f9b8f4b8d2455d502545f51b6
Arjoonn Sharma <arjoonn.94@gmail.com>
2017-10-24 17:38:08 +0530

    cleaner

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Missing Persons India
 
 Taken from the pdf available at [National Crime Records Bureau](http://ncrb.nic.in/MissingUidb/20170821-Missing%20Person%20Report.pdf).
 We use Tabula to extract the CSVs and perform post processing on top of that.
+
+1. Extract multiple csvs and store in `csvs` folder.
+2. Run `python joiner.py`
+3. Run `python cleaner.py`
--- cleaner.py
+++ cleaner.py
@@ -1,4 +1,22 @@
 import pandas as pd
 
 df = pd.read_csv('data.csv')
-print(df.info())
+mapping = {'NORMAL/MEDIU': 'normalmedium',
+           'THIN': 'thin',
+           'STRONG': 'strong',
+           'FAT': 'fat',
+           '2.0NORMAL/MEDIU': 'normalmedium',
+           'VERYLANKY(SKELETAL)': 'verylanky',
+           '83.0THIN': 'thin',
+           '53.0THIN': 'thin',
+           'MUSCULAR': 'muscular',
+           'VERYFAT': 'veryfat',
+           '22.0THIN': 'thin',
+           '14.0THIN': 'thin',
+           '22.0NORMAL/MEDIU': 'normalmedium',
+           '1.0NORMAL/MEDIU': 'normalmedium',
+           '1.0THIN': 'thin',
+           '83.0NORMAL/MEDIU': 'normalmedium'
+           }
+df.Built = df.Built.map(mapping)
+df.to_csv('cleandata.csv', index=False)
